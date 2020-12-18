2c75005fb14eb3ba9aacbdc13d99a354826af297
Agung <agung.siswanto@fastratabuana.co.id>
2020-12-18 13:41:51 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer();
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("i'm middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("i another middleware");
+});
+
+const server = http.createServer(app);
 
 server.listen(3010);
